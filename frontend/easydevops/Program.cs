e6f5d3b843402c8a54ba0b5f06b00ab59feafaf7
cdemmer04 <chiel.demmer@gmail.com>
2024-11-25 19:01:03 +0100

    .

---
+++ frontend/easydevops/Program.cs
@@ -0,0 +1,9 @@
+﻿// Zie https://aka.ms/new-console-template voor meer informatie
+
+Console.WriteLine("Welkom bij EasyDevOps!");
+
+Console.WriteLine(@"
+ ITM
+");
+
+Console.WriteLine("Huidige tijd: " + DateTime.Now);
